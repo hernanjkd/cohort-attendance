c6e0d9232af4880e01742f138c5acca05b22bb0f
hernanjkd <hernanjkd@gmail.com>
2019-07-08 15:28:20 -0400

    first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,35 +1,290 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			demo: [
+			students: [
 				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
+					name: "Bob Dylan",
+					assistance: [
+						{
+							date: new Date("6/2/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/3/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/4/2019"),
+							in: 920,
+							out: 1800
+						},
+						{
+							date: new Date("6/5/2019"),
+							in: 1000,
+							out: 1700
+						},
+						{
+							date: new Date("6/6/2019"),
+							in: 900,
+							out: 1500
+						},
+						{
+							date: new Date("6/7/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/8/2019"),
+							in: 1000,
+							out: 1500
+						}
+					]
 				},
 				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
+					name: "Rafael Luca",
+					assistance: [
+						{
+							date: new Date("6/2/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/3/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/4/2019"),
+							in: 920,
+							out: 1800
+						},
+						{
+							date: new Date("6/5/2019"),
+							in: 1000,
+							out: 1700
+						},
+						{
+							date: new Date("6/6/2019"),
+							in: 900,
+							out: 1500
+						},
+						{
+							date: new Date("6/7/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/8/2019"),
+							in: 1000,
+							out: 1500
+						}
+					]
+				},
+				{
+					name: "Marcos Arro",
+					assistance: [
+						{
+							date: new Date("6/2/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/3/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/4/2019"),
+							in: 920,
+							out: 1800
+						},
+						{
+							date: new Date("6/5/2019"),
+							in: 1000,
+							out: 1700
+						},
+						{
+							date: new Date("6/6/2019"),
+							in: 900,
+							out: 1500
+						},
+						{
+							date: new Date("6/7/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/8/2019"),
+							in: 1000,
+							out: 1500
+						}
+					]
+				},
+				{
+					name: "Clara Sanchi",
+					assistance: [
+						{
+							date: new Date("6/2/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/3/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/4/2019"),
+							in: 920,
+							out: 1800
+						},
+						{
+							date: new Date("6/5/2019"),
+							in: 1000,
+							out: 1700
+						},
+						{
+							date: new Date("6/6/2019"),
+							in: 900,
+							out: 1500
+						},
+						{
+							date: new Date("6/7/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/8/2019"),
+							in: 1000,
+							out: 1500
+						}
+					]
+				},
+				{
+					name: "Noelia Ramirez",
+					assistance: [
+						{
+							date: new Date("6/2/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/3/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/4/2019"),
+							in: 920,
+							out: 1800
+						},
+						{
+							date: new Date("6/5/2019"),
+							in: 1000,
+							out: 1700
+						},
+						{
+							date: new Date("6/6/2019"),
+							in: 900,
+							out: 1500
+						},
+						{
+							date: new Date("6/7/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/8/2019"),
+							in: 1000,
+							out: 1500
+						}
+					]
+				},
+				{
+					name: "Humberto Lopez",
+					assistance: [
+						{
+							date: new Date("6/2/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/3/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/4/2019"),
+							in: 920,
+							out: 1800
+						},
+						{
+							date: new Date("6/5/2019"),
+							in: 1000,
+							out: 1700
+						},
+						{
+							date: new Date("6/6/2019"),
+							in: 900,
+							out: 1500
+						},
+						{
+							date: new Date("6/7/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/8/2019"),
+							in: 1000,
+							out: 1500
+						}
+					]
+				},
+				{
+					name: "Raquel Rio",
+					assistance: [
+						{
+							date: new Date("6/2/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/3/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/4/2019"),
+							in: 920,
+							out: 1800
+						},
+						{
+							date: new Date("6/5/2019"),
+							in: 1000,
+							out: 1700
+						},
+						{
+							date: new Date("6/6/2019"),
+							in: 900,
+							out: 1500
+						},
+						{
+							date: new Date("6/7/2019"),
+							in: 900,
+							out: 1800
+						},
+						{
+							date: new Date("6/8/2019"),
+							in: 1000,
+							out: 1500
+						}
+					]
 				}
 			]
 		},
-		actions: {
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
+		actions: {}
 	};
 };
 
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,19 +1,35 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Context } from "../store/appContext";
 
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button, bootstrap is working
-				</a>
-			</div>
+			<Context.Consumer>
+				{({ store }) => {
+					return (
+						<div className="text-center mt-5">
+							<h1>Hello Rigo!</h1>
+							<p>
+								{store.students[0].assistance.map((item, index) => {
+									return (
+										<div key={index}>
+											<div>{String(item.date)}</div>
+											<div>{item.in}</div>
+											<div>{item.out}</div>
+											<div>{String((item.out - item.in) / 9).substr(0, 5)}%</div>
+										</div>
+									);
+								})}
+								{/* {console.log(store.students)} */}
+							</p>
+							<a href="#" className="btn btn-success">
+								If you see this green button, bootstrap is working
+							</a>
+						</div>
+					);
+				}}
+			</Context.Consumer>
 		);
 	}
 }
