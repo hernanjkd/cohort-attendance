39a34248cb2c92eb7453184026606fc95a0340f5
hernanjkd <hernanjkd@gmail.com>
2019-07-15 17:23:36 -0400

    fixing bugs

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -7,12 +7,12 @@ const getState = ({ setStore }) => {
 				fetch(url, { cache: "no-cache" })
 					.then(response => response.json())
 					.then(data => {
-						let students = data.data.filter(
-							e =>
-								(!e.first_name.length && !e.last_name.length) ||
-								!e.last_namee.first_name.includes("null")
-						);
-						students = data.data.map(e => {
+						// let students = data.data.filter(
+						// 	e =>
+						// 		(!e.first_name.length && !e.last_name.length) ||
+						// 		!e.last_namee.first_name.includes("null")
+						// );
+						let students = data.data.map(e => {
 							return {
 								// id: e.id,
 								// // Have all names with the same format
@@ -23,7 +23,7 @@ const getState = ({ setStore }) => {
 								// 	e.last_name.charAt(0).toUpperCase() +
 								// 	e.last_name.toLowerCase().slice(1),
 								data: e,
-								name: "machu"
+								name: e.first_name + " " + e.last_name;
 							};
 						});
 						setStore({ students: students });
