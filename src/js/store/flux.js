fd2ad358d03e45569ec1a3c401aa2b14d3995af3
hernanjkd <hernanjkd@gmail.com>
2019-07-16 09:30:28 -0400

    continue name fix

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -14,19 +14,17 @@ const getState = ({ setStore }) => {
 							// first_name: "null null", last_name: ""
 							if (e.first_name.includes("null") && e.last_name == "") return { ...e, name: e.email };
 
-							// first_name: "JohnDoe", last_name: null
 							let newName = "";
-							if (!e.first_name.includes(" ") && e.last_name == null)
-								for (let char of e.first_name)
-									newName += char == char.toUpperCase() ? " " + char : char;
-							else {
-								// first_name: "John doe", last_name: null
-								// first_name: "john JIMMY", last_name: "JOE Doe"
-								let nameArr = e.first_name.split(" ");
-								if (e.last_name != null) nameArr = nameArr.concat(e.last_name.split(" "));
+							let first = "";
+							// first_name: "JohnDoe", last_name: ""
+							for (let char of e.first_name) first += char == char.toUpperCase() ? " " + char : char;
 
-								for (let name of nameArr) newName += " " + capitalize(name);
-							}
+							// first_name: "John doe", last_name: null
+							// first_name: "john JIMMY", last_name: "JOE Doe"
+							let nameArr = first.split(" ");
+							if (e.last_name != null) nameArr = nameArr.concat(e.last_name.split(" "));
+							for (let name of nameArr) newName += " " + capitalize(name);
+
 							return { ...e, name: newName.slice(1) };
 						});
 						setStore({ students: students });
