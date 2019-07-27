07d8282165780db746627562765d70086e462a60
hernanjkd <hernanjkd@gmail.com>
2019-07-27 00:57:31 -0400

    completing project

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -22,7 +22,7 @@ const injectContext = PassedComponent => {
 			const access_token = "42ebf0ca4ad7d20eb340027343680d25ad6c2c26";
 			const availableEndPoints = ["students", "user"];
 
-			const get = availableEndPoints[1];
+			const get = availableEndPoints[0];
 			const url = `https://api.breatheco.de/${get}/?access_token=${access_token}`;
 			fetch(url, { cache: "no-cache" })
 				.then(response => response.json())
@@ -32,70 +32,70 @@ const injectContext = PassedComponent => {
 						/**************************************************************** */
 						/**************************************************************** */
 						/**************************************************************** */
-						const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-						const getUserName = email => email.substring(0, email.indexOf("@")).toLowerCase();
-						const fullTrim = str => {
-							let newStr = "";
-							str = str.trim();
-							for (let i in str) if (str[i] !== " " || str[i - 1] !== " ") newStr += str[i];
-							return newStr;
-						};
-						let first = e.first_name;
-						let last = e.last_name;
-						if (last === null) last = "";
-						// In the fetch url, Students have email, Users have username
-						let username = e.username === undefined ? getUserName(e.email) : getUserName(e.username);
+						// const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+						// const getUserName = email => email.substring(0, email.indexOf("@")).toLowerCase();
+						// const fullTrim = str => {
+						// 	let newStr = "";
+						// 	str = str.trim();
+						// 	for (let i in str) if (str[i] !== " " || str[i - 1] !== " ") newStr += str[i];
+						// 	return newStr;
+						// };
+						// let first = e.first_name;
+						// let last = e.last_name;
+						// if (last === null) last = "";
+						// // In the fetch url, Students have email, Users have username
+						// let username = e.username === undefined ? getUserName(e.email) : getUserName(e.username);
 
-						// first_name: null
-						// first_name: "null null"
-						if (first === null || first.includes("null")) {
-							first = username;
-						}
-						// first === email username, keep lowercase
-						else if (first.toLowerCase() === username) {
-							first = username;
-						} else {
-							first = fullTrim(first);
-							last = fullTrim(last);
+						// // first_name: null
+						// // first_name: "null null"
+						// if (first === null || first.includes("null")) {
+						// 	first = username;
+						// }
+						// // first === email username, keep lowercase
+						// else if (first.toLowerCase() === username) {
+						// 	first = username;
+						// } else {
+						// 	first = fullTrim(first);
+						// 	last = fullTrim(last);
 
-							let arr = first.split(" ");
-							// first_name: "John"
-							// first_name: "JohnDoe"
-							// first_name: "JOHNDOE"
-							if (arr.length === 1) {
-								if (first !== first.toLowerCase() && first !== first.toUpperCase()) {
-									let temp = "";
-									for (let char of first) {
-										if (char === char.toUpperCase() && isNaN(char)) temp += " " + char;
-										else temp += char;
-									}
-									first = temp.trim();
-									arr = first.split(" ");
-									if (arr.length === 1) first = capitalize(arr[0]);
-								} else first = capitalize(first);
-							}
-							// first_name: "john doe", last_name: ""
-							if (arr.length === 2 && last === "") {
-								first = capitalize(arr[0]);
-								last = capitalize(arr[1]);
-							}
-							// first_name: "john joe doe", last_name: ""
-							else if (arr.length === 3 && last === "") {
-								// first = capitalize(arr[0]) + " " + capitalize(arr[1]);
-								// last = capitalize(arr[2]);
-							}
-							// first_name: "john billy", last_name: "joe doe"
-							else if (last !== "") {
-								let arrl = last.split(" ");
-								for (let i in arr) arr[i] = capitalize(arr[i]);
-								for (let i in arrl) arrl[i] = capitalize(arrl[i]);
-								first = arr.join(" ");
-								last = arrl.join(" ");
-							}
-						}
+						// 	let arr = first.split(" ");
+						// 	// first_name: "John"
+						// 	// first_name: "JohnDoe"
+						// 	// first_name: "JOHNDOE"
+						// 	if (arr.length === 1) {
+						// 		if (first !== first.toLowerCase() && first !== first.toUpperCase()) {
+						// 			let temp = "";
+						// 			for (let char of first) {
+						// 				if (char === char.toUpperCase() && isNaN(char)) temp += " " + char;
+						// 				else temp += char;
+						// 			}
+						// 			first = temp.trim();
+						// 			arr = first.split(" ");
+						// 			if (arr.length === 1) first = capitalize(arr[0]);
+						// 		} else first = capitalize(first);
+						// 	}
+						// 	// first_name: "john doe", last_name: ""
+						// 	if (arr.length === 2 && last === "") {
+						// 		first = capitalize(arr[0]);
+						// 		last = capitalize(arr[1]);
+						// 	}
+						// 	// first_name: "john joe doe", last_name: ""
+						// 	else if (arr.length === 3 && last === "") {
+						// 		// first = capitalize(arr[0]) + " " + capitalize(arr[1]);
+						// 		// last = capitalize(arr[2]);
+						// 	}
+						// 	// first_name: "john billy", last_name: "joe doe"
+						// 	else if (last !== "") {
+						// 		let arrl = last.split(" ");
+						// 		for (let i in arr) arr[i] = capitalize(arr[i]);
+						// 		for (let i in arrl) arrl[i] = capitalize(arrl[i]);
+						// 		first = arr.join(" ");
+						// 		last = arrl.join(" ");
+						// 	}
+						// }
 
-						e.first_name = first;
-						e.last_name = last;
+						// e.first_name = first;
+						// e.last_name = last;
 						/**************************************************************** */
 						/**************************************************************** */
 						/**************************************************************** */
