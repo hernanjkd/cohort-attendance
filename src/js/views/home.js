a0cb1a64834d8c7833d176bb6e226aa7180c7f53
hernanjkd <hernanjkd@gmail.com>
2020-05-30 23:59:53 -0400

    update months

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,21 +2,6 @@ import React, { useState } from "react";
 import { Context } from "../store/appContext";
 import Popover from "../component/popover";
 
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-
 export const Home = props => {
 	const [zoom, setZoom] = useState("font-size-10px");
 
